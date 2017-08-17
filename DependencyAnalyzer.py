a2025f6cf3719d3b69f30a8d135b1d99a6b4922f
Carlos Arenas <carlos.arenas@zemoga.com>
2017-08-16 17:40:21 -0700

    Getting all the functions and subs from all the parent node references.

--- DependencyAnalyzer.py
+++ DependencyAnalyzer.py
@@ -1,21 +1,16 @@
-import os #os module imported here
+import os
+import json
 
 location = '/Users/carlosarenas/Dropbox/Work/Zemoga/Projects/Roku/Hulu/cube-roku'
-counter = 0 #keep a count of all files found
 xmlfiles = [] #list to store all XML files found at location
 parentNode = "BasePage"
-stringToFind = "extends=\"" + parentNode + "\""
 basePageFiles = []
-numberBasePageFiles = 0
+scriptsFunctions = {}
 
 def findChildNodes():
-    global location
-    global counter
-    global xmlfiles
-    global parentNode
-    global stringToFind
-    global basePageFiles
-    global numberBasePageFiles
+    counter = 0 #keep a count of all files found
+    numberBasePageFiles = 0
+    stringToFind = "extends=\"" + parentNode + "\""
 
     #Navigate through all the folders and files for a given location
     for root, dirs, files in os.walk(location, topdown=False):
@@ -35,9 +30,9 @@ def findChildNodes():
                         #Open the file and look to see if it extends from BasePage
                         lines = [line.rstrip('\n') for line in open(filePath)]
                         if any(stringToFind in s for s in lines):
-                            basePageFiles.append(str(file))
+                            basePageFiles.append(filePath)
                             numberBasePageFiles += 1
-                            print str(file) + " extends from " + parentNode
+                            # print str(file) + " extends from " + parentNode
                 except Exception as e:
                     raise e
                     print "No files found here!"
@@ -50,21 +45,67 @@ def findChildNodes():
     # print basePageFiles
 
 def dependenciesForParentNode():
-    global location
     #Dependencies used in the parent node
-    paretnNodeLocation = '/Users/carlosarenas/Dropbox/Work/Zemoga/Projects/Roku/Hulu/cube-roku/components/base/BasePage.xml'
+    paretnNodeLocation = location + '/components/base/BasePage.xml'
     fileContent = [line.rstrip('\n') for line in open(paretnNodeLocation)]
     scripts = [s for s in fileContent if "<script type=\"text/brightscript\" uri=" in s]
     #Go through all the different scripts and look for all the different fuctions in such file
+    scriptUri = "pkg:"
+    scriptExtension = ".brs"
     for script in scripts:
-        startScript = script.find("pkg:")
-        endScript = script.find(".brs")
-        scriptToAnalize = location + script[startScript+4:endScript+4]
+        startScript = script.find(scriptUri)
+        endScript = script.find(scriptExtension)
+        scriptName = script[startScript + len(scriptUri) : endScript + len(scriptExtension)]
+        scriptToAnalize = location + scriptName
         # print "SCRIPT TO ANALYZE: " + scriptToAnalize
         scriptContent = [line.rstrip('\n') for line in open(scriptToAnalize)]
+        # print "-------------------------- CURRENT SCRIPT: " + scriptName + " --------------------------"
+
+        #Loop throug all the functions
         functions = [s for s in scriptContent if "function " in s.lower()]
-        print functions
+        functionList = []
+        for function in functions:
+            function = cleanString(function)
+            if function[:8] == "function":
+                startFuction = function.find("function")
+                endFuction = function.find("(")
+                functionName = function[startFuction + 8 : endFuction]
+                # print functionName
+                functionList.append(str(functionName))
 
+        #Loop throug all the subroutines
+        subroutines = [s for s in scriptContent if "sub " in s.lower()]
+        subList = []
+        for sub in subroutines:
+            sub = cleanString(sub)
+            if sub[:3] == "sub":
+                startSub = sub.find("sub")
+                endSub = sub.find("(")
+                subName = sub[startSub + 3 : endSub]
+                # print subName
+                subList.append(str(subName))
 
-# findChildNodes()
-dependenciesForParentNode()
+        scriptsFunctions['script_name:'] = scriptName
+        scriptsFunctions['script_functions:'] = functionList
+        scriptsFunctions['script_subs:'] = subList
+
+        print "---------------------------------------------------------"
+        # print scriptsFunctions
+        print json.dumps(scriptsFunctions)
+
+def functionsUsedByChildren():
+    for script in basePageFiles:
+        lines = [line.rstrip('\n') for line in open(filePath)]
+        for line in lines:
+            # for functions in arreglo de diccionario
+
+def cleanString(stringToClean):
+    stringToClean = stringToClean.strip()
+    stringToClean = stringToClean.lstrip()
+    stringToClean = stringToClean.rstrip()
+    stringToClean = stringToClean.replace(" ", "")
+    stringToClean = stringToClean.lower()
+    return stringToClean
+
+findChildNodes()
+# dependenciesForParentNode()
